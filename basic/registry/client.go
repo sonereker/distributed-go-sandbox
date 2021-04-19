51324b74744497ffdc5b1dcbd2f59056a4cd5597
Soner Eker <sonereker@gmail.com>
2021-04-19 10:13:33 +0300

    Update providers
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- basic/registry/client.go
+++ basic/registry/client.go
@@ -4,13 +4,23 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"sync"
 )
 
 func RegisterService(r Registration) error {
+	serviceUpdateUrl, err := url.Parse(r.ServiceUpdateURL)
+	if err != nil {
+		return err
+	}
+	http.Handle(serviceUpdateUrl.Path, &serviceUpdateHandler{})
+
 	buf := new(bytes.Buffer)
 	enc := json.NewEncoder(buf)
-	err := enc.Encode(r)
+	err = enc.Encode(r)
 	if err != nil {
 		return err
 	}
@@ -26,6 +36,26 @@ func RegisterService(r Registration) error {
 	return nil
 }
 
+type serviceUpdateHandler struct{}
+
+func (suh serviceUpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	dec := json.NewDecoder(r.Body)
+	var p patch
+	err := dec.Decode(&p)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	prov.Update(p)
+}
+
 func ShutdownService(serviceURL string) error {
 	req, err := http.NewRequest(http.MethodDelete, ServicesURL, bytes.NewBuffer([]byte(serviceURL)))
 	if err != nil {
@@ -37,4 +67,49 @@ func ShutdownService(serviceURL string) error {
 		return fmt.Errorf("failed to deregister service. Registry service responded with code %v", res.StatusCode)
 	}
 	return err
+}
+
+type providers struct {
+	services map[ServiceName][]string
+	mutex    *sync.RWMutex
+}
+
+func (p providers) Update(pat patch) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	for _, patchEntry := range pat.Added {
+		if _, ok := p.services[patchEntry.Name]; !ok {
+			p.services[patchEntry.Name] = make([]string, 0)
+		}
+		p.services[patchEntry.Name] = append(p.services[patchEntry.Name], patchEntry.URL)
+	}
+
+	for _, patchEntry := range pat.Removed {
+		if providerURLs, ok := p.services[patchEntry.Name]; ok {
+			for i := range providerURLs {
+				if providerURLs[i] == patchEntry.URL {
+					p.services[patchEntry.Name] = append(providerURLs[:i], providerURLs[i+1:]...)
+				}
+			}
+		}
+	}
+}
+
+func (p providers) get(name ServiceName) (string, error) {
+	providers, ok := p.services[name]
+	if !ok {
+		return "", fmt.Errorf("no providers available for %v", name)
+	}
+	idx := int(rand.Float32() * float32(len(providers)))
+	return providers[idx], nil
+}
+
+func GetProvider(name ServiceName) (string, error) {
+	return prov.get(name)
+}
+
+var prov = providers{
+	services: make(map[ServiceName][]string),
+	mutex:    new(sync.RWMutex),
 }
