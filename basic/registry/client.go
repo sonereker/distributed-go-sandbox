06dd5953f535e58c980b7c07e433515838fa624f
Soner Eker <sonereker@gmail.com>
2021-04-19 11:21:21 +0300

    Notify service when a dependency started
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- basic/registry/client.go
+++ basic/registry/client.go
@@ -53,6 +53,7 @@ func (suh serviceUpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	fmt.Printf("Update received %+v\n", p)
 	prov.Update(p)
 }
 
--- basic/registry/server.go
+++ basic/registry/server.go
@@ -23,10 +23,46 @@ func (r *registry) add(reg Registration) error {
 	r.registrations = append(r.registrations, reg)
 	r.mutex.Unlock()
 	err := r.sendRequiredServices(reg)
-	if err != nil {
-		return err
+	r.notify(patch{
+		Added: []patchEntry{
+			{Name: reg.ServiceName, URL: reg.ServiceURL},
+		},
+	})
+	return err
+}
+
+func (r *registry) notify(fullPatch patch) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+
+	for _, reg := range r.registrations {
+		go func(reg Registration) {
+			for _, reqService := range reg.RequiredServices {
+				p := patch{Added: []patchEntry{}, Removed: []patchEntry{}}
+				sendUpdate := false
+
+				for _, added := range fullPatch.Added {
+					if added.Name == reqService {
+						p.Added = append(p.Added, added)
+						sendUpdate = true
+					}
+				}
+				for _, removed := range fullPatch.Removed {
+					if removed.Name == reqService {
+						p.Removed = append(p.Removed, removed)
+						sendUpdate = true
+					}
+				}
+				if sendUpdate {
+					err := r.sendPatch(p, reg.ServiceUpdateURL)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+				}
+			}
+		}(reg)
 	}
-	return nil
 }
 
 func (r *registry) sendRequiredServices(reg Registration) error {
